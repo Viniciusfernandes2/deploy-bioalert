3d5a75c117e9618c74d2525541779b070509bb92
Viniciusfernandes2 <vinicius.fernandes11.vf@gmail.com>
2025-11-18 04:20:58 -0300

    deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,19 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import router from './routes'; // importa o arquivo backend/routes/index.ts
+import router from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// todas as rotas vão estar sob o prefixo /api
+// Prefixo geral das rotas
 app.use('/api', router);
 
+// Porta dinâmica (Render) ou 3000 local
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
 
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+});
